922088f41da3a30db6545a33caadaf2b7968d0c4
Yassin <mac@MacBook-Pro.local>
2021-10-06 17:59:40 +0100

    Initial commit

---
+++ controllers/SiteController.php
@@ -0,0 +1,21 @@
+<?php
+namespace app\controllers;
+
+/**
+*
+ *
+ * @package app\controllers
+ */
+
+
+class SiteController
+{
+    public function contact()
+    {
+        return 'Handling submitted data';
+    }
+    public function handleContact()
+    {
+        return 'Handling submitted data';
+    }
+}
---
+++ views/contact.php
@@ -0,0 +1,23 @@
+<h1>Contact us</h1>
+
+
+<form action="" method="post">
+    <div class="mb-3">
+        <label>Subject</label>
+        <input type="text" name="subject" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label>Email</label>
+        <input type="Email" name="Email" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label>Body</label>
+        <textarea name="body" class="form-control"></textarea>
+    </div>
+    <div class="mb-3">
+        <label>Subject</label>
+        <input type="text" name="subject" class="form-control">
+    </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
