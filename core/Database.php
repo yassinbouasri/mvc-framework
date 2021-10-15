c75a7a7be6d959f5471bad9fcb4248829524f86e
yassinbouasri <yassinbouasri@gmail.com>
2021-10-15 03:14:39 +0100

    env file & Application config

--- core/Database.php
+++ core/Database.php
@@ -4,7 +4,7 @@ namespace app\core;
 
 class Database
 {
-    public \PDO $PDO;
+    public \PDO $pdo;
 
     public function __construct(array $config)
     {
@@ -12,7 +12,36 @@ class Database
         $user = $config['user'] ?? '';
         $password = $config['password'] ?? '';
 
-        $this->PDO = new \PDO($dsn, $user, password);
-        $this->PDO->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $this->pdo = new \PDO($dsn, $user, $password);
+        $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function applyMigrations()
+    {
+        $this->createMigrationsTable();
+        $this->getApplaiedMigrations();
+
+        $files = scandir(Application::$ROOT_DIR, 'migrations');
+
+        HelperFunctions::dd($files);
+    }
+
+    public function createMigrationsTable()
+    {
+        $this->pdo->exec("CREATE TABLE IF NOT EXISTS migrations (
+                            id INT AUTO_INCREMENT PRIMARY KEY,
+                            migration VARCHAR(255),
+                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP        
+    
+                                ) ENGINE=INNODB;
+                           ");
+    }
+
+    public function getApplaiedMigrations()
+    {
+        $statment = $this->PDO->prepare("SELECT migration FROM migrations");
+        $statment->execute();
+
+        return $statment->fetchAll(\PDO::FETCH_COLUMN);
     }
 }
